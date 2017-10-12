d41756893c526e68ec2df3c4b1ae1ebe2c207026
Ricardo Wurmus <rekado@elephly.net>
2017-10-12 18:36:36 +0200

    Add Guix environment file.
    * guix.scm: New file.

---
+++ guix.scm
@@ -0,0 +1,53 @@
+;;; hpcguix-web - Web interface for Guix
+;;; Copyright © 2017 Ricardo Wurmus <rekado@elephly.net>
+;;;
+;;; This file is part of hpcguix-web.
+;;;
+;;; hpcguix-web is free software; see COPYING file for details.
+
+;;; Run the following command to enter a development environment for
+;;; HPC Guix web:
+;;;
+;;;  $ guix environment -l guix.scm
+
+(use-modules ((guix licenses) #:prefix license:)
+             (guix packages)
+             (guix download)
+             (guix utils)
+             (guix build-system gnu)
+             (gnu packages)
+             (gnu packages autotools)
+             (gnu packages guile)
+             (gnu packages lisp)
+             (gnu packages package-management)
+             (gnu packages pkg-config))
+
+(define %version
+  (symbol->string (with-input-from-file "VERSION" read)))
+
+(package
+  (name "hpcguix-web")
+  (version %version)
+  (source (string-append (getcwd) "/hpcguix-web-" version ".tar.gz"))
+  (build-system gnu-build-system)
+  (arguments
+   `(#:phases
+     (modify-phases %standard-phases
+       (add-before 'configure 'autoconf
+         (lambda _
+           (zero? (system* "autoreconf" "-vif")))))))
+  (native-inputs
+   `(("autoconf" ,autoconf)
+     ("automake" ,automake)
+     ("uglify-js" ,uglify-js)
+     ("pkg-config" ,pkg-config)))
+  (inputs
+   `(("guix" ,guix)
+     ("guile" ,guile-2.2)))
+  (home-page "https://github.com/UMCUGenetics/hpcguix-web")
+  (synopsis "Web interface for cluster deployments of Guix")
+  (description "This package provides a web interface to the list of
+packages provided by Guix.  The list of packages is searchable and
+provides instructions on how to use Guix in a shared HPC
+environment.")
+  (license license:agpl3+))
