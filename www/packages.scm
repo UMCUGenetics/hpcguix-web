UMCUGenetics/hpcguix-web

commit 88b07caaf3084f48b181c3f3f84af27dc9b834ec

Explicitly terminate inferiors.
Previously we would accumulate inferiors (child processes) but would not
call 'waitpid' on them, leaving zombies behind and/or leading to
deadlocks when the corresponding pipes get closed asynchronously.

* www/packages.scm (update-package-file): Add call to 'close-inferior'.

www/packages.scm
@@ -102,6 +102,7 @@ Guix packages."
                                vlist-null
                                packages))
 
+        (close-inferior inferior)
         (return #t)))))
 
 (define* (maybe-update-package-file file channels

commit 0244333a8c2af7f6fb37130537d9952125de19ab

Keep the current inferior around.
Fixes a bug introduced in 88b07caaf3084f48b181c3f3f84af27dc9b834ec where
/package/* GET requests would fail because the inferior associated with
<inferior-package> instances would be closed already.

* www/packages.scm (current-inferior): New variable.
(update-package-file): Use 'atomic-box-swap!' to set 'current-inferior';
close the previous inferior but keep the current one live.

www/packages.scm
@@ -39,6 +39,12 @@
   ;; packages.
   (make-atomic-box vlist-null))
 
+(define current-inferior
+  ;; Current inferior.  It is kept around so that requests such as
+  ;; 'inferior-package-description' that involve communication with the
+  ;; inferior can succeed.
+  (make-atomic-box #f))
+
 (define (package-synopsis-shtml package)
   "Return an SXML representation of PACKAGE synopsis field with HTML
 vocabulary."
@@ -85,7 +91,13 @@ Guix packages."
                            vlist-null
                            packages))
 
-    (close-inferior inferior)))
+    ;; Keep INFERIOR around so that operations on CURRENT-PACKAGES, such as
+    ;; calls to 'inferior-package-description', succeed (there's a time
+    ;; window during which this is out-of-sync compared to CURRENT-PACKAGES,
+    ;; but it doesn't matter much.)
+    (let ((previous (atomic-box-swap! current-inferior inferior)))
+      (when previous
+        (close-inferior previous)))))
 
 (define* (maybe-update-package-file file channels
                                     #:key (select? (const #t))